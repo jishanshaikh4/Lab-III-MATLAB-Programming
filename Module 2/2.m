9c802ce6bdb5f24974f0bbaa951d68709abd9b8e
Jishan Shaikh <30091032+Jishanshaikh4@users.noreply.github.com>
2018-11-07 20:54:09 +0530

    Update 2.m

--- Module 2/2.m
+++ Module 2/2.m
@@ -1,5 +1,7 @@
 % Programs from index 4, 5, and 6
 
+% Program number 4
+
 N = input('input n:');
 X = input('input x:');
 sum_series = Series(N,X);
@@ -12,6 +14,7 @@ function [Sum]= Series(N,X)
   	end
 end
 
+% Program number 5
 
 a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 b = [];
@@ -25,6 +28,7 @@ end
 fprintf("%d ",b);
 fprintf("\n");
 
+% Program number 6
 
 N = input("Enter the number: ");
 a =['zero';'one';'two';'three';'four';'five';'six';'seven';'eight';'nine'];
